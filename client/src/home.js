2b1842a4a3f418780fc5872ee9a1b3cc96089177
samgos <samuel.jj.gosling@gmail.com>
2019-06-26 04:54:37 +0200

    metamask manual enquiry and web3 nesting

--- client/src/home.js
+++ client/src/home.js
@@ -52,7 +52,7 @@ class Home extends Component {
 
  render() {
   return (
-    <div>
+    <div className="homeNavigation">
       <Landing/>
       <PageOne/>
       <PageTwo/>
--- client/src/utils/tokenMetadata.js
+++ client/src/utils/tokenMetadata.js
@@ -2,7 +2,7 @@ import ERC20d from "../contracts/ERC20d.json";
 import Web3 from "web3";
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-const metadataObject = { data: [], sum: 0, count: 0, avg: 0 };
+const metadataObject = { data: [], sum: 0, avg: 0 };
 
  function parseValue(_value) {
   return convertHex(web3.utils.toBN(convertHex(_value)).div(web3.utils.toBN(convertHex(1e18))));
@@ -32,36 +32,28 @@ async function averageTransfer(_instance) {
        var transferAmount = parseValue(event.returnValues.value);
        metaData.sum += parseFloat(transferAmount);
        metaData.data.push(transferAmount);
-       metaData.count++;
-     }
-  }); if(metaData.sum !== 0 || metaData.count !== 0){
-    metaData.avg = metaData.sum/metaData.data.length;
-  } else {
-    metaData.avg = 0;
-  } return metaData;
+    }
+  });
+  return metaData;
 }
 
 const tokenMetadata = () =>
   new Promise(async(resolve, reject) => {
-      const networkId = await web3.eth.net.getId();
+      var networkId = await web3.eth.net.getId();
       var tokenContract = ERC20d.networks[networkId];
       var tokenInstance = new web3.eth.Contract(ERC20d.abi,
         tokenContract && tokenContract.address,
       );
       tokenInstance.setProvider(web3.currentProvider)
       var volumeObject = await tokenInstance.methods.volume.call();
+      var tokenVelocity = convertHex(divideValue(volumeObject[1], 200000));
       var indexData = new Date(convertHex(volumeObject[0])*1000);
-      var tokenVelocity = divideValue(volumeObject[1], 200000);
       var txObject = await averageTransfer(tokenInstance);
-      var totalTransactions = parseNumber(txObject.count);
       var weeklyVolume = parseNumber(volumeObject[1]);
-      var averageValue = parseNumber(txObject.avg);
       var weeklyIndex = createTimestamp(indexData);
       resolve([{
-        totalTransactions,
         weeklyVolume,
         tokenVelocity,
-        averageValue,
         weeklyIndex
       }])
 });
