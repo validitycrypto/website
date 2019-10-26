6da6798cbcb26a5c490c98cbf6580c113d86e1ae
samgos <samuel.jj.gosling@gmail.com>
2019-10-26 02:22:50 +0200

    state verification

--- client/src/navigation/register.js
+++ client/src/navigation/register.js
@@ -1,6 +1,8 @@
 import React, { Fragment, Component } from "react";
 import ReactGA from "react-ga";
+import firebase from "firebase"
 
+import { firebaseConfiguration } from "../utils/firebaseConfig"
 import { faTelegramPlane } from "@fortawesome/free-brands-svg-icons"
 import { faEnvelope, faBullhorn } from"@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -15,6 +17,7 @@ import Button from "@atlaskit/button"
 import "../assets/css/register.css";
 
 const ZERO = "0x0000000000000000000000000000000000000000";
+const TOKEN = "0x293b7712fb8e57e8637287f18b61945be78b8e27";
 
 class Register extends Component {
   constructor(props){
@@ -24,8 +27,10 @@ class Register extends Component {
  }
 
  componentWillMount = () => {
+   const firebaseDb = firebase.initializeApp(firebaseConfiguration("verify"), "Verify")
    ReactGA.pageview('/Register');
    this.setState({
+     firebaseDb: firebaseDb.firestore(),
      componentPhase: this.phaseOne()
    })
  }
@@ -36,16 +41,31 @@ class Register extends Component {
    const tokenContract = ERC20d.networks[metaMask.network];
    const tokenInstance = new metaMask.web3.eth.Contract(ERC20d.abi,
      tokenContract && tokenContract.address,
-   ); tokenInstance.options.address = "0x904da022abcf44eba68d4255914141298a7f7307";
+   ); tokenInstance.options.address = TOKEN;
    await this.setState({
      network: metaMask.network,
      account: accounts[0],
      token: tokenInstance,
      web3: metaMask.web3,
-   }); await this.getId();
-   this.setState({
-     componentPhase: this.phaseTwo()
-   });
+   }); await this.networkCheck(metaMask.network);
+ }
+
+ networkCheck = async(_network) => {
+   if(_network !== 4){
+     this.setState({
+       componentPhase: this.renderError("Wrong Network!")
+     })} else {
+      var stateValidity = await this.checkState(this.state.account);
+
+      if(stateValidity){ this.setState({
+         componentPhase: this.renderError("You've already been verified!")
+      })} else if(!stateValidity) {
+        await this.getId();
+        this.setState({
+          componentPhase: this.phaseTwo()
+      });
+     }
+   }
  }
 
  getId = async() => {
@@ -57,23 +77,52 @@ class Register extends Component {
  }
 
  generateId = async() => {
-   await new Promise((resolve, reject) =>
+   return await new Promise((resolve, reject) =>
      this.state.token.methods.conformIdentity()
      .send({ from: this.state.account, gas: 3725000})
      .on('confirmation', (confirmationNumber, receipt) => {
        if(confirmationNumber === 1){
          ReactGA.event({ category: 'Transactional',
          action: 'conformIdentity', label: 'True' })
-         this.setState({
-           componentPhase: this.phaseThree()
-         }); this.getId();
          resolve(receipt);
        }
      }).on('error', (error) => {
        ReactGA.event({ category: 'Transactional',
        action: 'conformIdentity', label: 'False' })
+       resolve(false);
      })
    )
+ }
+
+ checkState = async(_input) => {
+   const stateStatus = await this.state.firebaseDb.collection(_input)
+    .limit(1).get().then((result) => {
+     var outputValue;
+     result.forEach(item => {
+        outputValue = item.data().validity;
+      }); return outputValue;
+    }); if(stateStatus)return false;
+    else return true;
+  }
+
+ embedState = async(_input) => {
+   this.state.firebaseDb.collection(_input)
+    .add({
+      validity: true
+    });
+ }
+
+ executeSovergnity = async() => {
+   this.setState({
+     componentPhase: this.transactionPending()
+   }); var txValidity = await this.generateId();
+
+   if(txValidity !== false) {
+     await this.getId();
+     this.setState({
+       componentPhase: this.phaseThree()
+     });
+   }
  }
 
  verifyTweet = async(_tweetId) => {
@@ -93,10 +142,13 @@ class Register extends Component {
  }
 
  parseTweet = async(event) => {
-   const tweetId = event.target.value.split('/')[5];
+   var tweetId = event.target.value.split('/')[5];
+   var verifyState = await this.checkState(this.state.id);
    await this.verifyTweet(tweetId)
 
    if(this.state.validityId === this.state.id){
+     await this.embedState(this.state.validityId);
+     await this.embedState(this.state.account);
      this.setState({
        componentPhase: this.phaseFour()
      });
@@ -115,11 +167,19 @@ class Register extends Component {
   )
 }
 
+renderError = (_errorMessage) => {
+  return (
+   <Fragment>
+    <div className="errorTitle">{_errorMessage}</div>
+  </Fragment>
+  )
+}
+
 
  phaseFour = () => {
   return (
    <Fragment>
-    <div className="registrationTitle">Verified!</div>
+    <div className="verifiedTitle">Verified!</div>
   </Fragment>
   )
 }
@@ -129,13 +189,24 @@ phaseTwo = () => {
   return (
     <Fragment>
     <div className="registrationTitle">Generate an ValidityID</div>
-    <Button className="web3Button" onClick={this.generateId}>
+    <Button className="web3Button" onClick={this.executeSovergnity}>
      &nbsp;Generate&nbsp;
      </Button>
    </Fragment>
   )} else {
     return this.phaseThree();
   }
+}
+
+transactionPending = () => {
+ return (
+  <Fragment>
+   <div className="pendingTitle" color="#5aff9c">Transaction pending...</div>
+   <div className="verificationState">
+    <Loader size="Large" color="#ffa500" type="ball-triangle-path" active />
+   </div>
+ </Fragment>
+ )
 }
 
 
